f78b2708ded6097640275073f4fd997c5066a61f
ghsss <gabrielhsaldanha@gmail.com>
2025-04-05 22:46:05 -0300

    Created function to copy product code;
    Changed page name;
    
    Changed styles;
    
    Added button to redirect to WhatsApp;

--- src/pages/ProductMenu/components/ProductMenuList.tsx
+++ src/pages/ProductMenu/components/ProductMenuList.tsx
@@ -2,24 +2,46 @@ import React from 'react';
 import './styles/ProductMenuList.css';
 import ProductModel from '../../../models/Product';
 import ProductMenuListItem from './ProductMenuListItem';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface ProductMenuListProps {
   productMenuItems: ProductModel[],
   children?: React.ReactNode;
 }
 
+// const whatsAppQueryParams = encodeURIComponent('Olá!%20Gostaria%20de%20fazer%20um%20pedido.%20😁');;
+const whatsAppQueryParams = encodeURIComponent('Olá! Gostaria de fazer um pedido.');;
+
 function ProductMenuList({ productMenuItems }: ProductMenuListProps) {
+
+  // const goToWhatsApp() {
+  // href="https://api.whatsapp.com/send?phone=1111111111&text=Hi"
+  // }
+
+  // faWhatsapp
+
   return (
-    <div className="ProductMenuListContainer">
-      <h2>⭐ Cardápio ⭐</h2>
-      {
-        productMenuItems.map(productMenuItem => {
-          return (
-            <ProductMenuListItem item={productMenuItem} />
-          )
-        })
-      }
-    </div>
+    <>
+      <div className='row linksRow'>
+        <a rel='noreferrer' target='_blank' href={"https://wa.me/555499026453?text=" + whatsAppQueryParams}>{`Ir para o WhatsApp`}
+          <span style={{ marginLeft: `.5em` }}>
+            <FontAwesomeIcon fontSize={`3em`} color='green' icon={faWhatsapp} />
+          </span>
+        </a>
+        {/* <a href="https://wa.me/555499026453?text=Olá,%20Gostaria%20de%20fazer%20um%20pedido!">Fazer pedido pelo site</a> */}
+      </div>
+      <div className="ProductMenuListContainer">
+        <h2 style={{color: `#000`, fontWeight: `bold`, fontSize: `2em`}}>🔥 Cardápio Online 🔥</h2>
+        {
+          productMenuItems.map(productMenuItem => {
+            return (
+              <ProductMenuListItem item={productMenuItem} />
+            )
+          })
+        }
+      </div>
+    </>
   );
 }
 
