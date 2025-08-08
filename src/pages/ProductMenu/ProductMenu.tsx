39a409e21bb9237eb9aaceb3eb150ac18a166d02
ghsss <gabrielhsaldanha@gmail.com>
2025-08-08 17:48:58 -0300

    Cart redirect bugfix;

--- src/pages/ProductMenu/ProductMenu.tsx
+++ src/pages/ProductMenu/ProductMenu.tsx
@@ -794,7 +794,8 @@ function ProductMenu({ isAdmin, action, prefilledOrderChannel }: ProductMenuProp
 
   }
 
-  if ((selectedItem === null && !showCartPage) || (!showOrdersPage && !companyIsOpenNow())) {
+  if ((selectedItem === null && !showCartPage) ||
+    (selectedItem === null && !showCartPage && !showOrdersPage && !companyIsOpenNow())) {
 
     return (
       <div className="ProductMenuContainer">
